6939f1bda0458677b4478b9c7ce7b4921d7556e8
Asrul Azwan <asrulazwan90@gmail.com>
2023-09-18 22:43:47 +0800

    feat: add methods to interact with user API

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getUsers(): Observable<any> {
+    return this.http.get('http://localhost:3000/users');
+  }
+
+  getUserById(id: string): Observable<any> {
+    return this.http.get(`http://localhost:3000/users/${id}`);
+  }
+
+  createUser(user: any): Observable<any> {
+    return this.http.post('http://localhost:3000/users', user);
+  }
+
+  updateUser(id: string, user: any): Observable<any> {
+    return this.http.put(`http://localhost:3000/users/${id}`, user);
+  }
 }

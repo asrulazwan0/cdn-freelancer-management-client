c797f02df33c4ac70a4fbc080f665a2df7c70b4e
Asrul Azwan <asrulazwan90@gmail.com>
2023-09-19 10:32:39 +0800

    feat: Configure environment variables for API URL

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,30 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
+  private apiUrl = `${environment.apiUrl}/users`;
+
   constructor(private http: HttpClient) {}
 
   getUsers(): Observable<any> {
-    return this.http.get('http://localhost:3000/users');
+    return this.http.get(this.apiUrl);
   }
 
   getUserById(id: string): Observable<any> {
-    return this.http.get(`http://localhost:3000/users/${id}`);
+    return this.http.get(`${this.apiUrl}/${id}`);
   }
 
   createUser(user: any): Observable<any> {
-    return this.http.post('http://localhost:3000/users', user);
+    return this.http.post(this.apiUrl, user);
   }
 
   updateUser(id: string, user: any): Observable<any> {
-    return this.http.put(`http://localhost:3000/users/${id}`, user);
+    return this.http.put(`${this.apiUrl}/${id}`, user);
   }
 
   deleteUser(id: string): Observable<any> {
-    return this.http.delete(`http://localhost:3000/users/${id}`);
+    return this.http.delete(`${this.apiUrl}/${id}`);
   }
 }
